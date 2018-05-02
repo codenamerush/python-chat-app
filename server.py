0f27a5807c348bcff57a67fa4b8ce99e01f72db4
Aarushi Sharda <aarushi261096@gmail.com>
2018-05-03 00:15:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ from socket import *
 
 serverSocket = socket(AF_UNIX, SOCK_STREAM)
 serverSocket.bind("1111")
-serverSocket.listen(1)
+serverSocket.listen(8)
 print 'Server set up done'
 
+connectionSocket, addr = serverSocket.accept()
+	
 while True :
-	connectionSocket, addr = serverSocket.accept()
 	sentence = connectionSocket.recv(1024)
 	print 'From client:', sentence
 	capitalizedSentence = raw_input('Reply: ')
 	connectionSocket.send(capitalizedSentence)
-	serverSocket.listen(8)
 connectionSocket.close()
